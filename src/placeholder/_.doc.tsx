6840d1ff9f8986997f73ca3955b0ba05155c3496
franciscoblancojn <blancofrancisco34@gmail.com>
2024-11-11 11:45:39 -0500

    doc

---
+++ src/placeholder/_.doc.tsx
@@ -0,0 +1,21 @@
+import {FenextImgPlaceholder} from '.';
+
+export default {
+    id: "FenextImgPlaceholder",
+    name: "FenextImgPlaceholder",
+    description: "Imagen que se puede usar para mostrar un placeholder en caso de que la imagen original tenga un problema de carga.",
+    html:`
+        <img src="${FenextImgPlaceholder}" alt="" style={{width:"100%",maxWidth:"200px",}} />
+    `,
+      useExample: [
+        {
+          text: "Usar en la etiqueta img",
+          content: `<img src={FenextImgPlaceholder} alt="" />`
+        },
+        {
+          text: "Usar en la etiqueta Img de fenextjs",
+          content: `import { Img } from "fenextjs";
+<Img src={ImgUrl} alt="" imgIf404={FenextImgPlaceholder} />`
+        },
+      ]
+};
---
+++ src/user/_.doc.tsx
@@ -0,0 +1,21 @@
+import {FenextImgUserPlaceholder} from '.';
+
+export default {
+    id: "FenextImgUserPlaceholder",
+    name: "FenextImgUserPlaceholder",
+    description: "Imagen que se puede usar para mostrar un placeholder en caso de que la imagen del usuaro tenga un problema de carga.",
+    html:`
+        <img src="${FenextImgUserPlaceholder}" alt="" style={{width:"100%",maxWidth:"200px",}} />
+    `,
+      useExample: [
+        {
+          text: "Usar en la etiqueta img",
+          content: `<img src={FenextImgUserPlaceholder} alt="" />`
+        },
+        {
+          text: "Usar en la etiqueta Img de fenextjs",
+          content: `import { Img } from "fenextjs";
+<Img src={ImgUrl} alt="" imgIf404={FenextImgUserPlaceholder} />`
+        },
+      ]
+};
